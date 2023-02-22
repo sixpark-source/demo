9277602547b7f92289cb4c9cff646ba7170a0392
H <bluebat@gmail.com>
2023-02-22 16:29:15 +0800

    修改系统的用户登录和注册的账号为ez121

--- conf/conf.auth.php
+++ conf/conf.auth.php
@@ -5,7 +5,7 @@
 return [
     'client_id' => 'APPID',
     'client_secret' => 'CLIENT_ID',
-    'token_route' => 'https://auth.6park.com/index.php?app=token',
+    'token_route' => 'https://auth.ez121.com/index.php?app=token',
     'resource_host' => 'https://resource.e2bo.com',
-    'authorize_route' => 'https://auth.6park.com/index.php?app=authorize'
+    'authorize_route' => 'https://auth.ez121.com/index.php?app=authorize'
 ];
